fb204ee176e4075d5225794dac9125ad01b57d31
yuuki kajiyama <luv4u.1331@gmail.com>
2021-03-07 00:00:01 +0900

    fix.:Taskテーブルのカラム名変更・バリデーション追加

--- db/migrate/20210306104805_create_tasks.rb
+++ db/migrate/20210306104805_create_tasks.rb
@@ -1,9 +1,8 @@
 class CreateTasks < ActiveRecord::Migration[5.2]
   def change
     create_table :tasks do |t|
-      t.string :name, null: false
-      t.text :detail
-
+      t.string :title, null: false
+      t.text :content
       t.timestamps
     end
   end
