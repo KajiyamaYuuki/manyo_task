80c65687de75e38f1be128ded4a85d9daa2ffa91
yuuki kajiyama <luv4u.1331@gmail.com>
2021-03-07 19:02:23 +0900

    feat.:Task新規作成機能追加

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,13 +1,33 @@
 class TasksController < ApplicationController
+  before_action :set_task, only: %i[show, edit]
   def index
+    @task = Task.all
   end
 
   def new
+    @task = Task.new
   end
 
-  def show
+  def create
+    @task = Task.new(task_params)
+    if @task.save
+      redirect_to tasks_path
+    else
+      render :new
+    end
   end
 
-  def edit
+  def show; end
+
+  def edit; end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:title, :content)
+  end
+
+  def set_task
+    @task = Task.find([:id])
   end
 end
