a5c08af01dd2a4fe7ec42be3ddcc3ec0f22d410f
yuuki kajiyama <luv4u.1331@gmail.com>
2021-03-12 14:01:54 +0900

    feat.:タスクに終了期限を追加して投稿できる機能の実装

--- db/migrate/20210311053529_add_columns_to_tasks.rb
+++ db/migrate/20210311053529_add_columns_to_tasks.rb
@@ -1,5 +1,5 @@
 class AddColumnsToTasks < ActiveRecord::Migration[5.2]
   def change
-    add_column :tasks, :expired_at, :timestamp, default: -> { 'CURRENT_TIMESTAMP' }, null: false
+    add_column :tasks, :expired_at, :datetime, default: -> { 'CURRENT_TIMESTAMP' }, null: false
   end
 end
