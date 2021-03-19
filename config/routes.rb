590e8904047f5e27a8cfae0c4b9729b8f17bfba4
yuuki kajiyama <luv4u.1331@gmail.com>
2021-03-19 15:08:47 +0900

    全体デザインの修正

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails
   .application
   .routes
   .draw do
-    root 'tasks#index'
+    root 'sessions#new'
     resources :tasks
     resources :sessions, only: [:new, :create, :destroy]
     resources :users, only: [:new, :create, :show]
