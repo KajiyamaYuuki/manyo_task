bb8531c217919e0f86cf28269e956532f0e21dcc
yuuki kajiyama <luv4u.1331@gmail.com>
2021-03-19 20:06:55 +0900

    feat.:モデルのコールバックの実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,14 @@ class User < ApplicationRecord
   validates :password, presence: true, length: { minimum: 6 }
   has_many :tasks, dependent: :destroy
   has_secure_password
+  before_update :must_not_update_last_admin_user
+  before_destroy :must_not_destroy_last_admin_user
+
+  def must_not_destroy_last_admin_user
+    throw(:abort) if User.where(admin: true).count == 1 && self.admin == true
+  end
+
+  def must_not_update_last_admin_user
+    throw(:abort) if User.where(admin: true).count == 1 && !self.admin && self.admin_was
+  end
 end
