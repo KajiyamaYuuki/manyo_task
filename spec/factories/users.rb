e7ff0a94bd45810d7dc3f7ba5a15f490d3ae650a
yuuki kajiyama <luv4u.1331@gmail.com>
2021-03-21 12:18:15 +0900

    test.:task_specを動作するよう修正

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -13,6 +13,34 @@ FactoryBot.define do
     admin { 'false' }
   end
 
+  factory :user3, class: User do
+    name { 'user3' }
+    email { 'user3@email.com' }
+    password { '33333333' }
+    admin { 'false' }
+  end
+
+  factory :user4, class: User do
+    name { 'user4' }
+    email { 'user4@email.com' }
+    password { '44444444' }
+    admin { 'false' }
+  end
+
+  factory :user5, class: User do
+    name { 'user5' }
+    email { 'user5@email.com' }
+    password { '55555555' }
+    admin { 'false' }
+  end
+
+  factory :user6, class: User do
+    name { 'user6' }
+    email { 'user6@email.com' }
+    password { '66666666' }
+    admin { 'false' }
+  end
+
   factory :admin_user, class: User do
     name { 'admin_user' }
     email { 'admin@email.com' }
