dd2a70c9a515290c55bb642d89f8bcccf8628a99
yuuki kajiyama <luv4u.1331@gmail.com>
2021-03-22 12:29:53 +0900

    feat.:seedデータ要件作成

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,20 +6,33 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-# User.create!(
-#   email: 'test@test.com',
-#   name: 'テスト太郎',
-#   password: 'testtest'
-# )
-
-# User.create!(
-#   name:  "管理者",
-#   email: "admin@example.jp",
-#   password:  "11111111",
-#   password_confirmation: "11111111",
-#   admin: true
-# )
-
-5.times do |i|
-  Label.create!(name: "sample#{i + 1}")
+10.times do |i|
+  Label.create!(name: "label#{i + 1}")
 end
+
+10.times do |i|
+  User.create!(
+    name: "user#{i + 1}",
+    email: "user#{i + 1}@email.com",
+    password: "user#{i + 1}user#{i + 1}",
+    password_confirmation: "user#{i + 1}user#{i + 1}",
+    admin: false
+  )
+end
+
+
+10.times do |i|
+  Task.create!(
+    title: "task#{i + 1}",
+    content: "task#{i + 1}",
+    user_id: User.find(i + 1).id
+  )
+end
+
+User.create!(
+  name:  "管理者",
+  email: "admin@example.jp",
+  password:  "11111111",
+  password_confirmation: "11111111",
+  admin: true
+)
